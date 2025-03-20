f6533e7f711c3f76e04fcb9f91d0b89ee00bb65a
Kanishka Kumar <kumarkanishka2111@gmail.com>
2025-03-20 11:35:44 +0530

    catch error

--- Backend/db/index.js
+++ Backend/db/index.js
@@ -2,17 +2,14 @@ import mongoose from "mongoose";
 import { DB_NAME } from "../utils/constants.js";
 
 const connectDB = async () => {
-    try{
+    try {
         const connectionInstance = await mongoose.connect(`${process.env.MONGO_URI}/${DB_NAME}`);
         console.log(`\n MongoDB connected successfully to host: ${connectionInstance.connection.host}`);
-
-
-    }
-    catch{
-        console.log("Error connecting to MongoDB", error);
+    } 
+    catch (error) {  
+        console.error("Error connecting to MongoDB:", error);
         process.exit(1);
     }
-}
-
+};
 
 export default connectDB;
